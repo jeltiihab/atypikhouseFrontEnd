a7835ea6b325b6a2b1dab6f1418ff5e9ca58e023
Agndiaye <44862115+agndiaye001@users.noreply.github.com>
2021-12-22 18:01:10 +0100

    'type Properties'

---
+++ src/components/TypeProperties/TypeProperties.js
@@ -0,0 +1,24 @@
+import styles from'./TypeProperties.module.css';
+
+
+
+// 
+function TypeProperties() {
+    return (
+        <div className={styles.container}>
+            <div className={styles.itemsContent}>1 itionitmsPositionitemsPosition</div>
+            <div className={styles.itemsContent}>1</div>
+            <div className={styles.itemsContent}>1</div>
+            <div className={styles.itemsContent}>1</div>
+            <div className={styles.itemsContent}>1</div>
+            <div className={styles.itemsContent}>1</div>
+            <div>1</div>
+            <div>1</div>
+            <div>1</div>
+            <div>1</div>
+            <div>1</div>
+        </div>
+    )
+}
+
+export default TypeProperties
