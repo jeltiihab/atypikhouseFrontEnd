9e773defc0a7b6b9a5063aebc664774b827f7bff
Agndiaye <44862115+agndiaye001@users.noreply.github.com>
2022-01-02 17:02:18 +0100

    ' Properties + infoCards + Property '

--- pages/property/index.js
+++ pages/property/index.js
@@ -10,28 +10,50 @@ import styles from "./property.module.css"
 import buttonStyle from "../../src/components/ui/Buttons/Buttons.module.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faStarHalfAlt, faMapMarkedAlt, faUser, faHeart } from '@fortawesome/free-solid-svg-icons'
+import property from "../../src/data/propertiesData";
+import { useRouter } from "next/router";
+
+
 
 const Property = () => {
     const [startDate, setStartDate] = useState(new Date());
     const [endDate, setendDate] = useState(new Date());
+   // const [property, setProperty] = useState();
+
+    
     const handleSelect = (ranges) => {
         setStartDate(ranges.selection.startDate);
         setendDate(ranges.selection.endDate)
     }
+
     const selectionRange = {
         startDate: startDate,
         endDate: endDate,
         key: "selection"
     };
+
+
+
+    // https://programmingwithmosh.com/javascript/axios-in-react-bring-your-data-to-the-front/
+    // get data after Click on "Nos Biens"
+    // const getProperty = async () => {
+    //     const id = this.props.match.params.id // we grab the ID from the URL
+    //     const {data} = await axios.get(`/properties/${id}`)
+    //     this.setProperty({property: data})       
+    // }
+
+
+      
     return (
             <div className={styles.container}>
                 <div className={styles.propertyTitle}>
-                    <h1>Cabane dans une arbre en normandie pour 3 personnes</h1>
+                    {/* <h1>Cabane dans une arbre en normandie pour 3 personnes</h1> */}
                 </div>
                 <div className={styles.infoBar}>
                     <div>
                         <div className={styles.infoBarLeftSide}>
-                            <div className={styles.infoBarItems}><label>3/5</label></div>
+                            <div className={styles.infoBarItems}><label>{property.rate}</label></div>
+                            {/* <div className={styles.infoBarItems}><label>3/5</label></div> */}
                             <div className={styles.infoBarItems}><FontAwesomeIcon className={styles.infoBarIcons} size="xs" icon={faMapMarkedAlt} /><label>Normandie</label></div>
                             <div className={styles.infoBarItems}><FontAwesomeIcon className={styles.infoBarIcons} size="xs" icon={faUser} /><label>User</label></div>
                         </div>
@@ -43,14 +65,15 @@ const Property = () => {
                         <div><Image className={styles.propertyPictures} src={"/../public/galerie1.jpg"} width={1000} height={500}/></div>
                         <div><Image className={styles.propertyPictures} src={"/../public/galerie2.jpg"} width={1000} height={500}/></div>
                         <div><Image className={styles.propertyPictures} src={"/../public/galerie3.jpg"} width={1000} height={500}/></div>
-                        <div><Image className={styles.propertyPictures} src={"/../public/galerie4.jpg"} width={1000} height={500}/></div>
+                        <div><Image className={styles.propertyPictures} src={"/../public/galerie4.jpg"} width={1000} height={500}/></div> 
                     </div>
                     <div>
                         <div><Image className={styles.propertyPictures} src={"/../public/galerie4.jpg"} width={1500} height={778}/></div>
                     </div>
                 </div>
                 <div className={styles.tenantInfos}>
-                    <h2>Cabane loué par <span>Jeremy XAVIER</span></h2>
+                    <h2>Cabane loué par <span>{property.name}</span></h2>
+                    {/* <h2>Cabane loué par <span>Jeremy XAVIER</span></h2> */}
                     <FontAwesomeIcon className={styles.infoBarIcons} icon={faUser} />
                 </div>
                 <hr/>
@@ -58,13 +81,17 @@ const Property = () => {
                     <div className={styles.propertyDescription}>
                         <h3>Description</h3>
                         <p>
+                            {property.description}
+                        </p>
+                        {/* <h3>Description</h3> */}
+                        {/* <p>
                             Situé à 2,1 km du château de la Motte Noszvaj, Le TreeHouses Noszvaj propose un service de prêt de vélos, un jardin et des hébergements climatisés dotés d'un balcon et d'une connexion Wi-Fi gratuite.
                             Chaque logement dispose d'une terrasse offrant une vue sur le lac, d'une télévision par satellite à écran plat, d'une cuisine bien équipée et d'une salle de bains privative avec bain à remous, sèche-cheveux et articles de toilette gratuits. Vous pourrez profiter d'un micro-ondes, d'un réfrigérateur, d'un grille-pain, d'une bouilloire et d'une machine à café.
                             Un petit-déjeuner anglais/irlandais complet est servi sur place.
                             L'espace bien-être du TreeHouses Noszvaj comprend un bain à remous et un sauna.
                             Vous pourrez pratiquer la randonnée et la pêche dans les environs.
                             Les couples apprécient particulièrement l'emplacement de cet établissement. Ils lui donnent la note de 9,4 pour un séjour à deux.
-                        </p>
+                        </p> */}
                     </div>
                     <div className={styles.reservationForm}>
                         <h4>Vérifier vos disponibilités et les prix</h4>
@@ -88,7 +115,8 @@ const Property = () => {
                             <h3>Equipements</h3>
                         </div>
                         <div className={styles.equipementsImages}>
-                            <Image src="/../public/icons/wifi.png" width={40} height={40} objectFit="contain"/>
+                            {/* <Image src={property.image} width={40} height={40} objectFit="contain"/>
+                            {/* <Image src="/../public/icons/wifi.png" width={40} height={40} objectFit="contain"/> */}
                             <Image src="/../public/icons/air-conditioner.png" width={40} height={40} objectFit="contain"/>
                             <Image src="/../public/icons/cable-car-cabin.png" width={40} height={40} objectFit="contain"/>
                             <Image src="/../public/icons/kayak.png" width={40} height={40} objectFit="contain"/>
