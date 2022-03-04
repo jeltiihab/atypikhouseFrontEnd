f4974c8880dc88f642964e0bbb9558e3db31aa03
jeltiihab <ihab.jelti@gmail.com>
2022-03-04 16:09:04 +0100

    Fix registration problems

--- pages/register/index.js
+++ pages/register/index.js
@@ -3,29 +3,24 @@ import WelcomBanner from "../../src/components/core/WelcomeBanner/WelcomeBanner"
 import styles from "../login/login.module.css"
 import ButtonStyle from '../../src/components/ui/Buttons/Buttons.module.css'
 import { Formik, Form, ErrorMessage } from 'formik';
-import * as yup from "yup"
 import axios from "axios";
 import DateControl from "../../src/components/ui/FormInputs/Datepicker"
 import Input from "../../src/components/ui/FormInputs/Input"
 import Select from "../../src/components/ui/FormInputs/Select"
 import CheckBox from "../../src/components/ui/FormInputs/CheckBox"
-import moment from 'moment'
+import { userRoleOptions, userSexe } from "../../utils/StaticData"
+import { userschema } from "../../utils/Schemas"
+import { useRouter } from 'next/router'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
 
 const Index = () => {
 
-    // Regex to verify the phone number
-    const phoneRegExp = /^((\\+[1-9]{1,4}[ \\-]*)|(\\([0-9]{2,3}\\)[ \\-]*)|([0-9]{2,4})[ \\-]*)*?[0-9]{3,4}?[ \\-]*[0-9]{3,4}?$/;
+    var CryptoJS = require("crypto-js");
+    
+    const Router = useRouter()
 
     // This options will be mapped to create inputs from components
-    const userRoleOptions = [
-        {key: 'Locataire', value: 'ROLE_USER'},
-        {key: 'Propriétaire', value: 'ROLE_PROP'}
-    ];
-    const userSexe = [
-        {key: 'Homme', value: 'homme'},
-        {key: 'Femme', value: 'femme'},
-        {key: 'Autre', value: 'autre'}
-    ];
     const checkBoxOptions = [
         {key: "J\'accepte la politique de confidentialité", value: 'terms'}
     ];
@@ -35,85 +30,43 @@ const Index = () => {
         userRole: "",
         firstName: "",
         lastName: "",
-        birthDay: "",
+        birthDate: "",
         sexe: "",
         phone: "",
         email: "",
         password: "",
-        confirmPassword: "",
+        password_confirmation: "",
         termsOfService: []
     };
 
-    // Adding the schema for User with Yup
-    const userschema = yup.object().shape({
-        userRole: yup.string().required(),
-        firstName: yup
-            .string().required("Le nom est un champ obligatoire").min(3, "Le nom doit comporter au moins 3 caractères"),
-        lastName: yup
-            .string()
-            .required("Le prénom est un champ obligatoire")
-            .min(3, "Le prénom doit comporter au moins 3 caractères"),
-        birthDay: yup
-            .string("Veuillez indiquer votre âge")
-            .required("Veuillez indiquer votre âge")
-            .test("Date de naissance", "Vous devez avoir au moins 18 ans", (value) => {
-                return moment().diff(moment(value), "years") >= 18;
-            })
-            .nullable()
-            /*.number()
-            .required("Veuillez indiquer votre âge")
-            .min(18, "Vous devez avoir au moins 18 ans")
-            .max(99, "Vous devez avoir au plus 99 ans")*/,
-        sexe: yup.string().required(),
-        phone: yup.string().matches(phoneRegExp, 'Le numéro de téléphone n\'est pas valide'),
-        email: yup
-            .string()
-            .email()
-            .required("L'e-mail est un champ obligatoire"),
-        password: yup
-            .string()
-            .required("Please enter your password")
-            .matches(
-                /^.*(?=.{8,})((?=.*[!@#$%^&*()\-_=+{};:,<.>]){1})(?=.*\d)((?=.*[a-z]){1})((?=.*[A-Z]){1}).*$/,
-                "Le mot de passe doit contenir au moins 8 caractères, une majuscule, un chiffre et un caractère spécial"
-            ),
-        confirmPassword: yup
-            .string()
-            .required("Veuillez confirmer votre mot de passe")
-            .when("password", {
-                is: password => (password && password.length > 0 ? true : false),
-                then: yup.string().oneOf([yup.ref("password")], "Le mot de passe ne correspond pas")
-            })
-        /*termsOfService: yup
-            .boolean()
-            .required("The terms and conditions must be accepted.")
-            .oneOf([true], "The terms and conditions must be accepted.")*/
-    });
-
     const handleOnSubmit = async (values) => {
         // Convert the date to dd-mm-yyyy format
-        const birthDay = values.birthDay;
+        const birthDate = values.birthDate;
         const FormatDate =
-            birthDay.getDate() +
+            birthDate.getDate() +
             "-" +
-            (birthDay.getMonth() + 1) +
+            (birthDate.getMonth() + 1) +
             "-" +
-            birthDay.getFullYear();
+            birthDate.getFullYear();
         // Assing data with converted date to data
-        const data = {...values, birthDay: FormatDate};
-        console.log(data);
+        const data = {...values, birthDate: FormatDate};
+        //console.log(data);
         // Making post http request
-        const response = await axios.post("http://localhost:8000/api/users", data)
+        const response = await axios.post("/register", data)
             .catch((err) => {
                 if(err && err.response)
                     console.log("Error", err);
             });
         // if there is a response and a data then success
-        if(response && response.data) {
-            console.log(response.data.message)
-            setSuccess(response.data.message);
+        if(response && response.data && response?.data?.access_token !== undefined) {
+            localStorage.setItem('access_token', "Bearer "+response.data.access_token)
+            localStorage.setItem('user', CryptoJS.AES.encrypt( JSON.stringify(response.data.data), "Bearer "+response.data.access_token ).toString())
+            Router.push('/emailverification')
+            //setSuccess(response.data.message);
+        } else {
+            toast.error("Une erreur est survenue veilleur vérifié votre saisie", {theme: 'colored'});
+            return;
         }
-        alert("OK")
     }
     /*const formik = useFormik ({
         initialValues: {
@@ -153,49 +106,51 @@ const Index = () => {
     });*/
 
     return (
-        <Formik
-            initialValues={{
-                ...initialValues
-            }}
-            validationSchema={userschema}
-            onSubmit={handleOnSubmit}
+        <div>
+            <Formik
+                initialValues={{
+                    ...initialValues
+                }}
+                validationSchema={userschema}
+                onSubmit={handleOnSubmit}
             >
-            {(formik,errors, touched )=> (
-                <div className={styles.loginContainer}>
-                    {/* START right side of the login page */}
-                    <div className={styles.leftSideRegister} style={{marginBottom: '40rem'}}>
-                        <div className={styles.welcomeMsg}>
-                            <h1 className={styles.AtypikhouseLabel}>Bonjour</h1>
-                            <h2>Bienvenue</h2>
-                            <Form className={styles.loginForm} autoComplete="off">
+                {(formik,errors, touched )=> (
+                    <div className={styles.loginContainer}>
+                        {/* START right side of the login page */}
+                        <div className={styles.leftSideRegister} style={{marginBottom: '40rem', height: '100vh'}}>
+                            <div className={styles.welcomeMsg}>
+                                <h1 className={styles.AtypikhouseLabel}>Bonjour</h1>
+                                <h2>Bienvenue</h2>
+                                <Form className={styles.loginForm} autoComplete="off">
                                     <div className={styles.formInputsContainer}>
-                                        <Input label="Nom" name="firstName" type="text"/>
-                                        <Input label="Prénom"  name="lastName" type="text"/>
+                                        <Input label="Nom" name="lastName" type="text"/>
+                                        <Input label="Prénom"  name="firstName" type="text"/>
                                         <div className={styles.middleInputContainer}>
                                             <Select label="Vous êtes ?"  name="userRole" options={userRoleOptions}/>
                                             <Select label="Sexe"  name="sexe" options={userSexe}/>
-                                            <DateControl label="Date de naissance"  name="birthDay"/>
+                                            <DateControl label="Date de naissance"  name="birthDate"/>
                                         </div>
                                         <Input label="Télephone"  name="phone" type="text"/>
                                         <Input label="Email"  name="email" type="email"/>
                                         <Input label="Mot de pass"  name="password" name="password" type="password"/>
-                                        <Input label="Confirmer le mot de pass"  name="confirmPassword" type="password"/>
+                                        <Input label="Confirmer le mot de pass"  name="password_confirmation" type="password"/>
                                     </div>
-                                <div className="flex justify-center my-6">
-                                    <CheckBox name="termsOfService" options={checkBoxOptions}/>
-                                </div>
+                                    <div className="flex justify-center my-6">
+                                        <CheckBox name="termsOfService" options={checkBoxOptions}/>
+                                    </div>
                                     <div>
                                         <button className={ButtonStyle.greenMdButton} type="submit">
                                             S'inscrire
                                         </button>
                                     </div>
-                            </Form>
+                                </Form>
+                            </div>
                         </div>
+                        <WelcomBanner/>
                     </div>
-                    <WelcomBanner/>
-                </div>
-                    )}
-        </Formik>
+                )}
+            </Formik>
+        </div>
     );
 };
 
