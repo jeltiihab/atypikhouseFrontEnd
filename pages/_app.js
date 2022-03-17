63a3fb198817025e3c2971a93e9dcdf9812efeab
jeltiihab <ihab.jelti@gmail.com>
2022-03-17 22:15:41 +0100

    Adding meta viewport - final seo commit

--- pages/_app.js
+++ pages/_app.js
@@ -66,6 +66,7 @@ function MyApp({ Component, pageProps }) {
                     rel="stylesheet"
                     href="https://cdnjs.cloudflare.com/ajax/libs/nprogress/0.2.0/nprogress.min.css"
                 />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0" />
             </Head>
             {exclusionArray.indexOf(location) < 0 && 
             <Navbar
