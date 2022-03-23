6a1bf3e0ca70304210aaa0dcd95bcdab30448059
jeltiihab <ihab.jelti@gmail.com>
2022-03-23 22:57:35 +0100

    refactor and try to resolve payment state

--- pages/paimentValidation/index.js
+++ pages/paimentValidation/index.js
@@ -13,14 +13,14 @@ import PropertyImage from '../../public/images/galerie1.jpg'
 import PaypalIcon from '../../public/icons/paypal.png'
 import VisaIcon from '../../public/icons/visa.png'
 import MasterCardIcon from '../../public/icons/maestro.png'
-import { useRouter } from "next/router";
+import {useRouter} from "next/router";
 import axios from "axios";
 import LoadingOverlay from 'react-loading-overlay';
-import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
+import {PayPalScriptProvider, PayPalButtons} from "@paypal/react-paypal-js";
 import FullPageLoader from "../../src/components/ui/Spinner/FullPageLoader";
 import AuthContext from "../../context/AuthContext";
 
-const paimentValidation = function() {
+const paimentValidation = function () {
 
 
     const router = useRouter();
@@ -29,149 +29,166 @@ const paimentValidation = function() {
     const arrival = router.query.arrival;
     const departure = router.query.departure;
     const nbrVoyager = router.query.nbrVoyager;
-    const [isloading , setisloading] = useState(true);
-    const [orderUrl , setorderUrl] = useState("");
-    const [confirmUrl , setconfirmUrl] = useState("");
-    const { currentRoute, setCurrentRoute } = useNavigation();
+    const [isloading, setisloading] = useState(true);
+    const [orderUrl, setorderUrl] = useState("");
+    const [confirmUrl, setconfirmUrl] = useState("");
+    const {currentRoute, setCurrentRoute} = useNavigation();
     const [propertyData, setPropertyData] = useState([]);
+    const titleRef = useRef("");
 
-	
 
     const dataVerify = {arrival: arrival, departure: departure, propertyId: id};
-    const getProperty = async ()  => {
-	
-        var response = await  axios.post(`/reservations/check`, dataVerify) ;
-		console.log('response.data',response.data);
-		setorderUrl(response.data.orderUrl);
-		setPropertyData(response.data) ;
-		setisloading(false)
-            
-        }
+    const getProperty = async () => {
+
+        var response = await axios.post(`/reservations/check`, dataVerify);
+        console.log('response.data', response.data);
+        setorderUrl(response.data.orderUrl);
+        setPropertyData(response.data);
+        setisloading(false)
+
+    }
 
     useEffect(getProperty, [isAuthenticated, isLoading])
 
     if (isLoading || !isAuthenticated) {
-        return <FullPageLoader />;
+        return <FullPageLoader/>;
     }
 
-function getOrder() {
-				 console.log("order url",orderUrl) ;
-                                var ddata = {arrival: arrival, departure: departure, propertyId: id, hosting_capacity: nbrVoyager};
-								console.log('ddata',ddata) ;
-                                return fetch (orderUrl,{
-                                    method : "POST",
-                                    body : JSON.stringify(ddata),
-									 headers: {
-                        'content-type': 'application/json',
-                       
-                        'Accept' : 'application/json',
-                        'Authorization' : localStorage.getItem('access_token') ? localStorage.getItem('access_token') : ''
-                    }
+    async function getOrder() {
+        console.log("order url", orderUrl);
+        var ddata = {arrival: arrival, departure: departure, propertyId: id, hosting_capacity: nbrVoyager};
+        console.log('ddata', ddata);
+        await fetch(orderUrl, {
+            method: "POST",
+            body: JSON.stringify(ddata),
+            headers: {
+                'content-type': 'application/json',
 
+                'Accept': 'application/json',
+                'Authorization': localStorage.getItem('access_token') ? localStorage.getItem('access_token') : ''
+            }
 
-                                }).then(function(res) {
-                                    return res.json();
-                                }).then(function (data){
-									localStorage.setItem('data',data)
+        }).then(function (res) {
+            return res.json();
+        }).then(function (data) {
+            localStorage.setItem('data', data)
+            console.log("This is me", data.confirmUrl)
+            titleRef.current = data.confirmUrl
+            setconfirmUrl(data.confirmUrl);
+            console.log("confirm url", confirmUrl);
+            console.log("confirm url with ref => ", titleRef);
+            console.log("order ID => ", data.orderID);
+            return data.orderID;
+        })
 
-                                     setconfirmUrl( data.confirmUrl);
-                                    setTimeout(()=>{},3000)
-                                    console.log("confirm url",confirmUrl);
-                                    return data.orderID;
-                                })
+    }
 
-}
     return (
-<LoadingOverlay
-  active={orderUrl == ""}
-  spinner
-  text='Loading ...'
-  >
-        <div className={styles.container}>
-            <div className={styles.validatePaimentcontainer}>
-                <div className={styles.leftRightSide}>
-                    <h2 className={styles.titles}>Demande de réservation</h2>
-                    <div className={styles.propertyInfos}>
-                        <div><Image className={styles.propertyPictures} src={PropertyImage} width={150} height={150}/> </div>
+        <LoadingOverlay
+            active={orderUrl == ""}
+            spinner
+            text='Loading ...'
+        >
+            <div className={styles.container}>
+                <div className={styles.validatePaimentcontainer}>
+                    <div className={styles.leftRightSide}>
+                        <h2 className={styles.titles}>Demande de réservation</h2>
+                        <div className={styles.propertyInfos}>
+                            <div><Image className={styles.propertyPictures} src={PropertyImage} width={150}
+                                        height={150}/></div>
+                            <div>
+                                <div>{propertyData?.property?.name}</div>
+                                <div className={styles.infoBarItems}><FontAwesomeIcon className={styles.infoBarStarIcon}
+                                                                                      size="xs" icon={faStarHalfAlt}/>
+                                    <label>3/5</label></div>
+                                <div className={styles.infoBarItems}><FontAwesomeIcon className={styles.infoBarIcons}
+                                                                                      size="xs"
+                                                                                      icon={faMapMarkedAlt}/><label>{propertyData?.property?.address?.city}</label>
+                                </div>
+                                <div className={styles.infoBarItems}><FontAwesomeIcon className={styles.infoBarIcons}
+                                                                                      size="xs"
+                                                                                      icon={faUser}/><label>{propertyData?.property?.user?.firstName} {propertyData?.property?.user?.lastName}</label>
+                                </div>
+                            </div>
+                            <div><FontAwesomeIcon className={styles.infoBarIcons} size="xs" icon={faHeart}/></div>
+                        </div>
+                        <h2>Votre voyage</h2>
                         <div>
-                            <div>{propertyData?.property?.name}</div>
-                            <div className={styles.infoBarItems}><FontAwesomeIcon className={styles.infoBarStarIcon} size="xs" icon={faStarHalfAlt} /> <label>3/5</label></div>
-                            <div className={styles.infoBarItems}><FontAwesomeIcon className={styles.infoBarIcons} size="xs" icon={faMapMarkedAlt} /><label>{propertyData?.property?.address?.city}</label></div>
-                            <div className={styles.infoBarItems}><FontAwesomeIcon className={styles.infoBarIcons} size="xs" icon={faUser} /><label>{propertyData?.property?.user?.firstName} {propertyData?.property?.user?.lastName}</label></div>
-                        </div>
-                        <div><FontAwesomeIcon className={styles.infoBarIcons} size="xs" icon={faHeart} /></div>
-                    </div>
-                    <h2>Votre voyage</h2>
-                    <div>
-                        <h4>Dates</h4>
-                        <div className={styles.reservationInfos}>
-                            <div>
-                                <p><label>{arrival}</label> - <label>{departure}</label></p>
+                            <h4>Dates</h4>
+                            <div className={styles.reservationInfos}>
+                                <div>
+                                    <p><label>{arrival}</label> - <label>{departure}</label></p>
+                                </div>
+                                <div className={styles.actionLink}>
+                                    <a href="#">Modifier</a>
+                                </div>
                             </div>
-                            <div className={styles.actionLink}>
-                                <a href="#">Modifier</a>
+                            <h4>Voyageurs</h4>
+                            <div className={styles.reservationInfos}>
+                                <div>
+                                    <p><label>{nbrVoyager} Adultes</label></p>
+                                </div>
+                                <div className={styles.actionLink}>
+                                    <a href="#">Modifier</a>
+                                </div>
                             </div>
-                        </div>
-                        <h4>Voyageurs</h4>
-                        <div className={styles.reservationInfos}>
-                            <div>
-                                <p><label>{nbrVoyager} Adultes</label></p>
+                            <h4>Prix</h4>
+                            <div className={styles.reservationInfos}>
+                                <div>
+                                    <p>
+                                        <label>{propertyData?.property?.price}</label> x <label>{propertyData?.days}</label>Nuits
+                                    </p>
+                                </div>
+                                <div className={styles.actionLink}>
+                                    <a href="#">{propertyData?.property?.price * propertyData?.days} Euro</a>
+                                </div>
                             </div>
-                            <div className={styles.actionLink}>
-                                <a href="#">Modifier</a>
-                            </div>
-                        </div>
-                        <h4>Prix</h4>
-                        <div className={styles.reservationInfos}>
-                            <div>
-                                <p><label>{propertyData?.property?.price}</label> x <label>{propertyData?.days}</label>Nuits</p>
-                            </div>
-                            <div className={styles.actionLink}>
-                                <a href="#">{ propertyData?.property?.price * propertyData?.days } Euro</a>
-                            </div>
-                        </div>
-                        <hr/>
-                        <div className={styles.reservationInfos}>
-                            <div>
-                                <p><label>Total</label></p>
-                            </div>
-                            <div className={styles.actionLink}>
-                                <a href="#">{ propertyData?.property?.price * propertyData?.days } Euro</a>
+                            <hr/>
+                            <div className={styles.reservationInfos}>
+                                <div>
+                                    <p><label>Total</label></p>
+                                </div>
+                                <div className={styles.actionLink}>
+                                    <a href="#">{propertyData?.property?.price * propertyData?.days} Euro</a>
+                                </div>
                             </div>
                         </div>
                     </div>
+                    <div className={styles.leftRightSide}>
+
+                        <h2 className={styles.titles}>Payer avec</h2>
+                        {orderUrl != "" ? (
+                            <PayPalScriptProvider options={{
+                                "client-id": "AYz2CwrBi8Tu5wdVqsd9IIs3ZdfN0C7cIkA0gczx_AquHaVQQIQJT2M4Neghd04Kje2At62p2ked1-Bu",
+                                currency: "EUR",
+                                commit: true
+                            }}>
+                                <PayPalButtons
+                                    createOrder={getOrder}
+                                    onApprove={() => {
+                                        console.log('CONFIRM URL', confirmUrl)
+                                        return fetch(titleRef.current, {
+                                            headers: {
+                                                'content-type': 'application/json',
+                                                'Accept': 'application/json',
+                                                'Authorization': localStorage.getItem('access_token') ? localStorage.getItem('access_token') : ''
+                                            }
+                                        }).then(function (res) {
+                                            return res.json()
+                                        }).then(function (data) {
+                                            router.push("paymentsuccess")
+                                        })
+                                    }}
+                                />
+                            </PayPalScriptProvider>) : (<br/>)}
+
+
+                    </div>
                 </div>
-                <div className={styles.leftRightSide}>
 
-                    <h2 className={styles.titles}>Payer avec</h2>
-			{ orderUrl != ""?(
-                    <PayPalScriptProvider options={{ "client-id":  "AYz2CwrBi8Tu5wdVqsd9IIs3ZdfN0C7cIkA0gczx_AquHaVQQIQJT2M4Neghd04Kje2At62p2ked1-Bu", currency: "EUR", commit:true}}>
-                        <PayPalButtons
-                            createOrder={getOrder}
-                            onApprove={() => {
-                                console.log('CONFIRM URL', confirmUrl)
-                                return fetch(confirmUrl,{
-                                    headers: {
-                                        'content-type': 'application/json',
-                                        'Accept' : 'application/json',
-                                        'Authorization' : localStorage.getItem('access_token') ? localStorage.getItem('access_token') : ''
-                                    }
-                                }).then(function (res) {
-                                    return res.json()
-                                }).then(function (data) {
-                                    router.push("paymentsuccess")
-                                })
-                            }}
-                        />
-                    </PayPalScriptProvider> ) :(<br/>) }
-
-
-                </div>
             </div>
 
-        </div>
-
-</LoadingOverlay>
+        </LoadingOverlay>
     );
 };
 
