87a73983e52a8a6802c71073f1dd8c2681deba00
jeltiihab <ihab.jelti@gmail.com>
2022-04-06 22:59:31 +0200

    Delete testimonial part

--- pages/home/index.js
+++ pages/home/index.js
@@ -93,7 +93,7 @@ const Home = () => {
                             loop: true,
                         }}
                         />
-                        <p>Venez nous rejoindre dans cette experience pour devenir un hôte Atypik et béneficiez de revenue supplémentaire</p>
+                        <p className={styles.boldText}>Venez nous rejoindre dans cette experience pour devenir un hôte Atypik et béneficiez de revenue supplémentaire</p>
                         <button className={buttonStyle.redLgButton}>
                             Devenir partenaire
                         </button>
@@ -120,163 +120,9 @@ const Home = () => {
                 </div>
             </section>
             </main>
-                <div className={styles.testimonialDiv}>
-                    <div className={styles.testimonialContainer}>
-                        <div className="w-full max-w-6xl mx-auto">
-                            <div className="text-center max-w-xl mx-auto">
-                                <h2 className={styles.Titles}>Ce que les gens disent. <br/> de notre site Web.</h2>
-                                <div className="text-center mb-10">
-                                    <span className="inline-block w-1 h-1 rounded-full bg-red-500 ml-1"></span>
-                                    <span className="inline-block w-3 h-1 rounded-full bg-red-500 ml-1"></span>
-                                    <span className="inline-block w-40 h-1 rounded-full bg-red-500"></span>
-                                    <span className="inline-block w-3 h-1 rounded-full bg-red-500 ml-1"></span>
-                                    <span className="inline-block w-1 h-1 rounded-full bg-red-500 ml-1"></span>
-                                </div>
-                            </div>
-                            <div className="-mx-3 md:flex items-start">
-                                <div className="px-3 md:w-1/3">
-                                    <div
-                                        className={styles.testimonialCard}>
-                                        <div className="w-full flex mb-4 items-center">
-                                            <div
-                                                className="overflow-hidden rounded-full w-10 h-10 bg-gray-50 border border-gray-200">
-                                                <img src="https://i.pravatar.cc/100?img=1" alt=""/>
-                                            </div>
-                                            <div className="flex-grow pl-3">
-                                                <h6 className="font-bold text-sm uppercase text-gray-600">Kenzie
-                                                    Edgar.</h6>
-                                            </div>
-                                        </div>
-                                        <div className="w-full">
-                                            <p className="text-sm leading-tight"><span
-                                                className="text-lg leading-none italic font-bold text-gray-400 mr-1">"</span>Lorem
-                                                ipsum dolor sit amet consectetur adipisicing elit. Quos sunt ratione
-                                                dolor exercitationem minima quas itaque saepe quasi architecto vel!
-                                                Accusantium, vero sint recusandae cum tempora nemo commodi soluta
-                                                deleniti.<span
-                                                    className="text-lg leading-none italic font-bold text-gray-400 ml-1">"</span>
-                                            </p>
-                                        </div>
-                                    </div>
-                                    <div
-                                        className={styles.testimonialCard}>
-                                        <div className="w-full flex mb-4 items-center">
-                                            <div
-                                                className="overflow-hidden rounded-full w-10 h-10 bg-gray-50 border border-gray-200">
-                                                <img src="https://i.pravatar.cc/100?img=2" alt=""/>
-                                            </div>
-                                            <div className="flex-grow pl-3">
-                                                <h6 className="font-bold text-sm uppercase text-gray-600">Stevie
-                                                    Tifft.</h6>
-                                            </div>
-                                        </div>
-                                        <div className="w-full">
-                                            <p className="text-sm leading-tight"><span
-                                                className="text-lg leading-none italic font-bold text-gray-400 mr-1">"</span>Lorem
-                                                ipsum, dolor sit amet, consectetur adipisicing elit. Dolore quod
-                                                necessitatibus, labore sapiente, est, dignissimos ullam error ipsam sint
-                                                quam tempora vel.<span
-                                                    className="text-lg leading-none italic font-bold text-gray-400 ml-1">"</span>
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="px-3 md:w-1/3">
-                                    <div
-                                        className={styles.testimonialCard}>
-                                        <div className="w-full flex mb-4 items-center">
-                                            <div
-                                                className="overflow-hidden rounded-full w-10 h-10 bg-gray-50 border border-gray-200">
-                                                <img src="https://i.pravatar.cc/100?img=3" alt=""/>
-                                            </div>
-                                            <div className="flex-grow pl-3">
-                                                <h6 className="font-bold text-sm uppercase text-gray-600">Tommie
-                                                    Ewart.</h6>
-                                            </div>
-                                        </div>
-                                        <div className="w-full">
-                                            <p className="text-sm leading-tight"><span
-                                                className="text-lg leading-none italic font-bold text-gray-400 mr-1">"</span>Lorem
-                                                ipsum dolor sit amet, consectetur adipisicing elit. Vitae, obcaecati
-                                                ullam excepturi dicta error deleniti sequi.<span
-                                                    className="text-lg leading-none italic font-bold text-gray-400 ml-1">"</span>
-                                            </p>
-                                        </div>
-                                    </div>
-                                    <div
-                                        className={styles.testimonialCard}>
-                                        <div className="w-full flex mb-4 items-center">
-                                            <div
-                                                className="overflow-hidden rounded-full w-10 h-10 bg-gray-50 border border-gray-200">
-                                                <img src="https://i.pravatar.cc/100?img=4" alt=""/>
-                                            </div>
-                                            <div className="flex-grow pl-3">
-                                                <h6 className="font-bold text-sm uppercase text-gray-600">Charlie
-                                                    Howse.</h6>
-                                            </div>
-                                        </div>
-                                        <div className="w-full">
-                                            <p className="text-sm leading-tight"><span
-                                                className="text-lg leading-none italic font-bold text-gray-400 mr-1">"</span>Lorem
-                                                ipsum dolor sit amet consectetur adipisicing elit. Architecto inventore
-                                                voluptatum nostrum atque, corrupti, vitae esse id accusamus dignissimos
-                                                neque reprehenderit natus, hic sequi itaque dicta nisi voluptatem!
-                                                Culpa, iusto.<span
-                                                    className="text-lg leading-none italic font-bold text-gray-400 ml-1">"</span>
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="px-3 md:w-1/3">
-                                    <div
-                                        className={styles.testimonialCard}>
-                                        <div className="w-full flex mb-4 items-center">
-                                            <div
-                                                className="overflow-hidden rounded-full w-10 h-10 bg-gray-50 border border-gray-200">
-                                                <img src="https://i.pravatar.cc/100?img=5" alt=""/>
-                                            </div>
-                                            <div className="flex-grow pl-3">
-                                                <h6 className="font-bold text-sm uppercase text-gray-600">Nevada
-                                                    Herbertson.</h6>
-                                            </div>
-                                        </div>
-                                        <div className="w-full">
-                                            <p className="text-sm leading-tight"><span
-                                                className="text-lg leading-none italic font-bold text-gray-400 mr-1">"</span>Lorem
-                                                ipsum dolor sit amet consectetur adipisicing elit. Nobis, voluptatem
-                                                porro obcaecati dicta, quibusdam sunt ipsum, laboriosam nostrum facere
-                                                exercitationem pariatur deserunt tempora molestiae assumenda nesciunt
-                                                alias eius? Illo, autem!<span
-                                                    className="text-lg leading-none italic font-bold text-gray-400 ml-1">"</span>
-                                            </p>
-                                        </div>
-                                    </div>
-                                    <div
-                                        className={styles.testimonialCard}>
-                                        <div className="w-full flex mb-4 items-center">
-                                            <div
-                                                className="overflow-hidden rounded-full w-10 h-10 bg-gray-50 border border-gray-200">
-                                                <img src="https://i.pravatar.cc/100?img=6" alt=""/>
-                                            </div>
-                                            <div className="flex-grow pl-3">
-                                                <h6 className="font-bold text-sm uppercase text-gray-600">Kris
-                                                    Stanton.</h6>
-                                            </div>
-                                        </div>
-                                        <div className="w-full">
-                                            <p className="text-sm leading-tight"><span
-                                                className="text-lg leading-none italic font-bold text-gray-400 mr-1">"</span>Lorem
-                                                ipsum dolor sit amet consectetur adipisicing elit. Voluptatem iusto,
-                                                explicabo, cupiditate quas totam!<span
-                                                    className="text-lg leading-none italic font-bold text-gray-400 ml-1">"</span>
-                                            </p>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+            <div style={{marginBottom: '50px'}}>
+
+            </div>
         </div>
     );
 };
